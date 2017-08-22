7dd7ad7b6f783cfc5db4b06a5d466c2bc09d2a91
Azam Rahman <azam3745@gmail.com>
2017-08-22 16:55:41 +0800

    Initial commit

---
+++ web/main.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2017, Azam Rahman. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+// You can find me at https://github.com/Tameflame
+
+import 'package:angular2/platform/browser.dart';
+
+import 'package:Test/app_component.dart';
+
+void main() {
+  bootstrap(AppComponent);
+}
